07929deb72d2b7fc9ed3a154d8a28e469c40a555
youness boufous <younesss.bfs@gmail.com>
2017-11-17 10:55:18 +0000

    fixe more Calendre

--- public/staff/pages/Calendrier.php
+++ public/staff/pages/Calendrier.php
@@ -34,5 +34,20 @@
 </div>
 
 
-<?php include(SHARED_PATH . '/Footer.php'); ?>    
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+
+<!-- copyright -->
+<div class="footer-w3layouts">
+    <!--<div class="container">-->
+    <div class="agile-copy">
+        <p>© 2017 Travel Adventure. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+    </div>
+
+    <div class="clearfix"></div>
+    <!--</div>-->
+</div>
+<!-- copyright -->
+<script type="text/javascript" src="<?php echo url_for('stylesheets/js/jquery-2.1.4.min.js');?>"></script>
+        <script type="text/javascript" src="<?php echo url_for('stylesheets/js/bootstrap.js');?>"></script>
+</body>
+
+</html>
